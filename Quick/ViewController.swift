c275efd46ccf06f62a19c4b4be7de8bf45f834a7
Bob Warwick <bob@wholepunk.com>
2018-01-10 17:24:13 -0800

    Initial Commit

---
+++ Quick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quick
+//
+//  Created by Bob Warwick on 2018-01-10.
+//  Copyright © 2018 Whole Punk Creators Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
